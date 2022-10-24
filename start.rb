e4fb8d40bd92472b6d012c7ae426cd209c2b793c
Alphy Gacheru <alphygacheru@gmail.com>
2022-10-24 19:49:48 +0300

    Fix linter errors

--- start.rb
+++ start.rb
@@ -1,20 +1,20 @@
 require './app'
 
 def start_app(app, selection)
-    case selection
-    when 1
-      app.list_all_books
-    when 2
-      app.list_all_people
-    when 3
-      app.create_person
-    when 4
-      app.create_book
-    when 5
-      app.create_rental
-    when 6
-      app.list_all_rentals
-    else
-      puts 'Kindly select from 1 to 6 only'
-    end
+  case selection
+  when 1
+    app.list_all_books
+  when 2
+    app.list_all_people
+  when 3
+    app.create_person
+  when 4
+    app.create_book
+  when 5
+    app.create_rental
+  when 6
+    app.list_all_rentals
+  else
+    puts 'Kindly select from 1 to 6 only'
   end
+end
